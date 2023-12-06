a2bccb98051c02d4fa1ca3c318becf289c57903f
Luis F K Andriolo <luisfandriolo@gmail.com>
2023-12-06 17:58:30 +0000

    update

--- custom-theme.js
+++ custom-theme.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $("li.tel-phone").remove();
 
     $("#cabecalho > .barra-inicial").after(
-      '<div class"barra-superior-secundaria"><span class="primeira-compra">Use o código <strong>QA6UEGSXU</strong> e ganhe 5% de desconto na sua primeira compra!</span></div>'
+      '<div class="barra-superior-secundaria"><span class="primeira-compra">Use o código <strong>QA6UEGSXU</strong> e ganhe 5% de desconto na sua primeira compra!</span></div>'
     );
-  }, 500);
+  }, 300);
 });
