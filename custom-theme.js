72602d328a2b999c36d26317b42461b771e49df2
Luis F. K. Andriolo <52325074+luisfkandriolo@users.noreply.github.com>
2023-12-06 17:48:42 +0000

    update

--- custom-theme.js
+++ custom-theme.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-  $("li.tel-phone").remove();
+  setTimeout(() => {
+    $("li.tel-phone").remove();
 
-  $(".canais-contato.span9").append(
-    '<li><span class="primeira-compra">Use o código <strong>QA6UEGSXU</strong> e ganhe 5% de desconto na sua primeira compra!</span></li>'
-  );
+    $(".canais-contato.span9").append(
+      '<li><span class="primeira-compra">Use o código <strong>QA6UEGSXU</strong> e ganhe 5% de desconto na sua primeira compra!</span></li>'
+    );
+  }, 500);
 });
