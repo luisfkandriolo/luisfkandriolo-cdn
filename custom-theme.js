1d76c23786f42ba2cc2bc3a6b639d848b354fefe
Luis F K Andriolo <luisfandriolo@gmail.com>
2023-12-06 17:50:43 +0000

    update

--- custom-theme.js
+++ custom-theme.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   setTimeout(() => {
     $("li.tel-phone").remove();
 
-    $(".canais-contato.span9").append(
+    $(".canais-contato.span9 > ul").append(
       '<li><span class="primeira-compra">Use o código <strong>QA6UEGSXU</strong> e ganhe 5% de desconto na sua primeira compra!</span></li>'
     );
   }, 500);
