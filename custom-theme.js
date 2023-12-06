72df640a473becd01b3cea917fd9ea52860a4a38
Luis F. K. Andriolo <52325074+luisfkandriolo@users.noreply.github.com>
2023-12-06 14:35:04 -0300

    Update custom-theme.js

--- custom-theme.js
+++ custom-theme.js
@@ -1,3 +1,6 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $('li.tel-phone').remove()
+
+    $('.canais-contato.span9')
+        .append('<li><span class="primeira-compra">Use o código <strong>QA6UEGSXU</strong> e ganhe 5% de desconto na sua primeira compra!</span><
 });
