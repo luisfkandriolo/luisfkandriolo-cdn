fc3ce6f4f3187129d1cb5199f286e68fb70e6c82
Luis F. K. Andriolo <52325074+luisfkandriolo@users.noreply.github.com>
2023-12-10 19:34:13 -0300

    Update dg-custom-theme-js.js

--- dg-custom-theme-js.js
+++ dg-custom-theme-js.js
@@ -15,17 +15,14 @@ $(document).ready(function () {
     $("li.action-item.support > div > ul > li:nth-child(1)").remove()
     $("div.span4.links-rodape.links-rodape-atendimento > ul > li:nth-child(1)").remove()
 
-    setInterval(() => {
-      if (!document.querySelector("body.pagina-produto")) return
-      
-      document.querySelectorAll(".keywords-tags li")?.forEach((item) => {
-        let keyword = item.innerText
-        let keywordSearch = keyword.replace(" ","+")
-  
-        let uri = "https://www.decoracoesgeek.com.br/buscar?q=" + keywordSearch
-  
-        item.innerHTML = `<a href="${uri}" style="text-decoration:none; color:inherit;">${keyword}</a>`
-      })
-    }, 300)
+    document.querySelectorAll(".keywords-tags li")?.forEach((item) => {
+      let keyword = item.innerText
+      let keywordSearch = keyword.replace(" ","+")
+
+      let uri = "https://www.decoracoesgeek.com.br/buscar?q=" + keywordSearch
+
+      item.innerHTML = `<a href="${uri}" style="text-decoration:none; color:inherit;">${keyword}</a>`
+    })
+    
   }, 300);
 });
