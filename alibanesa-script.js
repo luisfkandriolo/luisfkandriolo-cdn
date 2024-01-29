33f65f0a629641616db166a59537fece9e22064d
Luis F. K. Andriolo <52325074+luisfkandriolo@users.noreply.github.com>
2024-01-29 10:22:00 +0000

    fix multiple elements injection

--- alibanesa-script.js
+++ alibanesa-script.js
@@ -5,12 +5,14 @@
       let target = document.querySelector("#etapa1detalhes");
       if (!target) return;
 
-      target
-        .querySelector("#headingTwo")
-        .insertAdjacentHTML(
-          "beforeend",
-          '<button id="collapseAditionals"><svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 18 18"><path fill="#494c4e" d="M17.707 5.707l-8 8a1 1 0 0 1-1.414 0l-8-8A1 1 0 0 1 1 4h16a1 1 0 0 1 .924.617A.97.97 0 0 1 18 5a1 1 0 0 1-.293.707z"/></svg></button>'
-        );
+      if (!target.querySelector("#collapseAditionals")) {
+        target
+          .querySelector("#headingTwo")
+          .insertAdjacentHTML(
+            "beforeend",
+            '<button id="collapseAditionals"><svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 18 18"><path fill="#494c4e" d="M17.707 5.707l-8 8a1 1 0 0 1-1.414 0l-8-8A1 1 0 0 1 1 4h16a1 1 0 0 1 .924.617A.97.97 0 0 1 18 5a1 1 0 0 1-.293.707z"/></svg></button>'
+          );
+      }
     } catch (error) {
       clearInterval(changeDetails);
     }
