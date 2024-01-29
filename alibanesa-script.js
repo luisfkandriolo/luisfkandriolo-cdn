09462e58935dd19731d7d37e07f846a35a487950
Luis F. K. Andriolo <52325074+luisfkandriolo@users.noreply.github.com>
2024-01-29 10:14:25 +0000

    firts changes, added collapse button

--- alibanesa-script.js
+++ alibanesa-script.js
@@ -1,5 +1,14 @@
 (function () {
+  // change aditional details
+  const changeDetails = setInterval(() => {
+    let target = document.querySelector("#etapa1detalhes");
+    if (!target) return;
 
-  console.log('teste')
-
-})()
+    target
+      .querySelector("#headingTwo")
+      .insertAdjacentHTML(
+        '<button id="collapseAditionals"><svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 18 18"><path fill="#494c4e" d="M17.707 5.707l-8 8a1 1 0 0 1-1.414 0l-8-8A1 1 0 0 1 1 4h16a1 1 0 0 1 .924.617A.97.97 0 0 1 18 5a1 1 0 0 1-.293.707z"/></svg></button>',
+        "beforeend"
+      );
+  }, 500);
+})();
